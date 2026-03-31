363c8f24f45bd86939913c273158c9e754c42afd
HORKimhab <41143106+HORKimhab@users.noreply.github.com>
2026-03-31 16:08:56 +0700

    lesson: improve performance vscode setting and ep6 - thymeleaf varaible expressions

---
+++ src/main/java/rean_it/com/thymeleaf_springboot_tutorial/controller/UserController.java
@@ -0,0 +1,22 @@
+package rean_it.com.thymeleaf_springboot_tutorial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import rean_it.com.thymeleaf_springboot_tutorial.model.User;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user-variable-expression")
+    public String userVariableExpression(Model model) {
+        User user = new User("HKimhab", "hkimhab@gmail.com", "ADMIN", "Male");
+        model.addAttribute("user", user);
+
+        // Return the name of the Thymeleaf template to be rendered
+        // In this case, it will look for a template named "user-variable-expression.html"
+        return "user-variable-expression";
+    }
+
+}
